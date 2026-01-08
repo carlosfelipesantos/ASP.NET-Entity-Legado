1e13b5efa815e12799b8acf13eb6e752797b1d7d
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-01-08 15:25:56 -0300

    Criando Model

---
+++ EFCore.WebAPI/EFCore.WebAPI/Model/Batalha.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCore.WebAPI.Model
+{
+    public class Batalha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+
+    }
+}
